8c1607693a909b2dada5a1c90bbab851089793bf
kaaaaajin <119267797+kaaaaajin@users.noreply.github.com>
2023-01-15 08:33:41 +0000

    table_create

--- db/migrate/20230115042310_create_order_items.rb
+++ db/migrate/20230115042310_create_order_items.rb
@@ -3,6 +3,12 @@ class CreateOrderItems < ActiveRecord::Migration[6.1]
     create_table :order_items do |t|
 
       t.timestamps
+      t.integer :item_id
+      t.integer :order_id
+      t.integer :amount
+      t.integer :price
+      t.integer :making_status
+      
     end
   end
 end
