f5d43b7517e024cd693d919a1550525b2dad3ced
EC2 Default User <ec2-user@ip-172-31-4-178.ap-northeast-1.compute.internal>
2023-01-17 04:31:29 +0000

    Gem[kaminari]を導入しました

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,15 @@
 class Admin::CustomersController < ApplicationController
+    def index
+        @customers = Customer.page(params[:page]).per(10)
+    end
+    
+    def show
+    end
+    
+    def edit
+    end
+    
+    def update
+    end
+    
 end
