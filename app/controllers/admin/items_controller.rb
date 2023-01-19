4bcf96359115f99a70ad9b9443abf2aa040e324e
EC2 Default User <ec2-user@ip-172-31-34-226.ap-northeast-1.compute.internal>
2023-01-19 04:25:41 +0000

    test

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ItemsController < ApplicationController
+  before_action :authenticate_admin!
     
     def new
     @item = Item.new
@@ -8,9 +9,11 @@ class Admin::ItemsController < ApplicationController
   
   def create
     item = Item.new(item_params)
-    item.genre_id = genre.id
-    item.save
-    redirect_to admin_items_path
+    if item.save
+      redirect_to admin_items_path
+    else
+      render :new
+    end
     
   end
   
@@ -19,23 +22,29 @@ class Admin::ItemsController < ApplicationController
     
   end
   
+  def show
+    @item = Item.find(params[:id])
+    
+    
+  end
+  
   def edit
+    @genres = Genre.all
+    @item = Item.find(params[:id])
   
   end
   
   def update
-    
-  end
-  
-  def destroy
-  
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to admin_item_path(item.id)
   end
   
   
   private 
   
   def item_params
-    params.require(:item).permit(:image, :name, :introduction, :price)
+    params.require(:item).permit(:image, :name, :introduction, :price, :genre_id, :is_active)
   end
   
   
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,14 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :genre
   
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
+  validates :admin, inclusion: {in: [true, false]}
+  
+  def with_tax_price
+    (price*1.1).floor
+  end
+  
 end
