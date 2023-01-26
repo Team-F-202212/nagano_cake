94f582e1917b3fef59ff95213d840c5c7dcc3ced
AsanoYuta <119588850+AsanoYuta@users.noreply.github.com>
2023-01-26 04:45:37 +0000

    test

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,12 +6,12 @@ class Item < ApplicationRecord
   # validates :introduction, presence: true
   # validates :price, presence: true
   # validates :image, presence: true
-  
+
    def with_tax_price
-     (price*1.1).floor
+     (price * 1.1).floor
    end
   has_many :order_items
   has_many :orders, through: :order_items
-  
+
 
 end
